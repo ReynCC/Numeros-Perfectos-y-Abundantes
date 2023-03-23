064b49547d9b6c0763af0c0182d795ff59b38e82
Enzo Fernandez <94704882+ReynCC@users.noreply.github.com>
2023-03-23 16:58:05 -0300

    Add files via upload

---
+++ Funciones/FuncionPyA.js
@@ -0,0 +1,44 @@
+function abundante(n) {
+
+    n = i ;
+
+    let suma = 0 ;
+
+   for (let x = 0; x < n; x++) {
+
+        if (n % x == 0) {
+
+        suma += x ;
+
+        }
+
+   }
+
+   if (suma > n) {
+
+        return true ;      
+
+   }
+   
+}
+
+function perfecto(l){
+k = i ;
+
+let suma = 0 ;
+
+for (let h = 0; h < k; h++) {
+
+if (k % h == 0) {
+
+suma += h ;
+
+}
+if (suma == i) {
+
+return true ;      
+}
+}
+
+
+}
